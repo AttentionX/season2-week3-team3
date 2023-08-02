f67958f033d0fe3f86177e51a094619a9696e5a5
Sounho Chung <aschung01@snu.ac.kr>
2023-08-02 11:12:14 +0000

    ADD : Added chroma util functions

--- vectorstore.py
+++ vectorstore.py
@@ -1,9 +1,41 @@
+import os
+from typing import Dict, List
+from chromadb.db import Documents
 import chromadb
 from chromadb.utils import embedding_functions
+from dotenv import load_dotenv
+from dataclasses import dataclass
+
+load_dotenv()
 
 openai_ef = embedding_functions.OpenAIEmbeddingFunction(
-                api_key="YOUR_API_KEY",
+                api_key=os.environ["OPENAI_API_KEY"],
                 model_name="text-embedding-ada-002"
             )
+id=0
 
-vectorstore = chromadb.Client(embedding_functions=openai_ef)
+client = chromadb.PersistentClient(path="/data")
+
+collection = client.create_collection("papers")
+
+@dataclass
+class Document:
+    content: str
+    metadata: Dict
+
+def embed(documents: List[Document]):
+    id += 1
+    contents = [doc.content for doc in documents]
+    collection.add(
+        embeddings=openai_ef(contents),
+        documents=contents,
+        metadatas=[doc.metadata for doc in documents],
+        ids=id
+    )
+
+def query(q: str, filter: Dict, top_k: int=5):
+    return collection.query(
+        query_embeddings=openai_ef([q]),
+        n_results=top_k,
+        where=filter
+    )
