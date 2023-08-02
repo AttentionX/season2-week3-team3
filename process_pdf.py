45347a60bec47499f400c9159b092130b01b66c0
shovelingpig <alan.k@hpcnt.com>
2023-08-02 21:01:51 +0900

    Extract text and caption from pdf

--- process_pdf.py
+++ process_pdf.py
@@ -1,32 +1,38 @@
 import json
 import replicate
-import PyPDF2
+import pypdf
 
 
-def extract_images_from_pdf(pdf_path):
+def extract_text_from_pdf(pdf_path):
     image_paths = []
-    meta_data = []
+    image_meta_data = []
+    text_per_page = []
     with open(pdf_path, 'rb') as file:
-        pdf_reader = PyPDF2.PdfReader(file)
+        pdf_reader = pypdf.PdfReader(file)
         for page_num in range(len(pdf_reader.pages)):
             page = pdf_reader.pages[page_num]
+            text = page.extract_text()
+            text_per_page.append(text)
             for idx, image_file_object in enumerate(page.images):
                 with open(image_file_object.name, "wb") as fp:
                     fp.write(image_file_object.data)
                 image_paths.append(image_file_object.name)
-                meta_data.append({
+                image_meta_data.append({
                     "name": image_file_object.name,
                     "page_num": page_num,
                     "position_in_page": idx,
                 })
 
-    return image_paths, meta_data
+    return text_per_page, image_paths, image_meta_data
 
 
 def generate_caption(image_path):
     output = replicate.run(
-        "andreasjansson/blip-2:4b32258c42e9efd4288bb9910bc532a69727f9acd26aa08e175713a0a857a608",
-        input={"image": open(image_path, "rb")}
+        "gfodor/instructblip:ca869b56b2a3b1cdf591c353deb3fa1a94b9c35fde477ef6ca1d248af56f9c84",
+        input={
+            "image_path": open(image_path, "rb"),
+            "prompt": "describe the figure image in the deep learning paper.",
+        }
     )
     return output
 
@@ -38,13 +44,20 @@ def save_json_file(file_path, data):
 
 
 def main(pdf_path):
-    image_paths, meta_data = extract_images_from_pdf(pdf_path)
+    text_per_page, image_paths, image_meta_data = extract_text_from_pdf(pdf_path)
     for idx, image_path in enumerate(image_paths):
         caption = generate_caption(image_path)
-        meta_data[idx]["caption"] = caption
+        image_meta_data[idx]["caption"] = caption
+        print("-"*20)
         print(f"Caption for Image {idx}: {caption}")
 
-    save_json_file("meta_data.json", meta_data)
+        page_num = image_meta_data[idx]["page_num"]
+        text_per_page[page_num] += ("\nFigure. " + caption + "\n")
+    
+    save_json_file("text_per_page.json", text_per_page)
+    save_json_file("image_meta_data.json", image_meta_data)
+
+    print("Done!!!")
 
 
 if __name__ == "__main__":
